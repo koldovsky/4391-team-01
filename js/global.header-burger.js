300732422591ea4c55e33ce76c76af80341a1d50
Denis Gedz <denisgedz95@gmail.com>
2025-08-24 13:31:49 +0300

    burger menu script add

---
+++ js/global.header-burger.js
@@ -0,0 +1,27 @@
+// Get DOM elements
+console.log("test");
+const burger = document.querySelector(".burger-menu");
+const nav = document.querySelector(".header__nav");
+const menuLinks = document.querySelectorAll(".header__nav-link");
+
+// Toggle menu function
+function toggleMenu() {
+  burger.classList.toggle("is-active");
+  nav.classList.toggle("is-open");
+  document.body.classList.toggle("no-scroll");
+}
+
+// Close menu function
+function closeMenu() {
+  burger.classList.remove("is-active");
+  nav.classList.remove("is-open");
+  document.body.classList.remove("no-scroll");
+}
+
+// Add click event listener to burger button
+burger.addEventListener("click", toggleMenu);
+
+// Add click event listeners to all menu links
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
