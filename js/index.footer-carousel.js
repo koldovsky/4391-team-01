65a62412ee69cf841c7257008f0f140293001105
petro <p.bondaruk@smartunex.com>
2025-08-26 16:16:52 +0200

    add responsive view to carousel

--- js/index.footer-carousel.js
+++ js/index.footer-carousel.js
@@ -16,6 +16,10 @@ function showSlide(index) {
     if (window.matchMedia('(min-width: 768px)').matches) {
         const nextSlideIdx = (index + 1) % slides.length;
         track.innerHTML += slides[nextSlideIdx];
+        if (window.matchMedia('(min-width: 1200px)').matches) {
+            const nextSlideIdx = (index + 2) % slides.length;
+            track.innerHTML += slides[nextSlideIdx];
+        }
     }
 }
 
@@ -29,13 +33,16 @@ function footerCarouselPrevSlide() {
     showSlide(currentSlideIdx);
 }
 
-
 // show first slide immediately
 showSlide(currentSlideIdx);
-// setInterval(nextSlide, 3000);
+setInterval(footerCarouselNextSlide, 3000);
 
 const btnNext = document.querySelector(".carousel__button--next");
 btnNext.addEventListener("click", footerCarouselNextSlide);
 
 const btnPrev = document.querySelector(".carousel__button--prev");
 btnPrev.addEventListener("click", footerCarouselPrevSlide);
+
+window.addEventListener("resize", () => {
+    showSlide(currentSlideIdx)
+});
