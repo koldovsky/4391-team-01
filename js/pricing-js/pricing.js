f88dca54ce6818a180b34fa2f41af33ed8474aa9
Yura121222 <103771161+Yura121222@users.noreply.github.com>
2025-08-27 20:44:55 +0000

    add theme toggle

--- js/pricing-js/pricing.js
+++ js/pricing-js/pricing.js
@@ -1,15 +1,16 @@
 function init() {
-    import('./pricing.main.info.partial.js');
-    import('./pricing.custom-plan-list.js');
-    import('../global.header-burger.js');
+  import("./pricing.main.info.partial.js");
+  import("./pricing.custom-plan-list.js");
+  import("../global.header-burger.js");
+  import("./theme-toggle.js")
 }
 
 const totalPartials = document.querySelectorAll(
-    '[hx-trigger="load"], [data-hx-trigger="load"]'
+  '[hx-trigger="load"], [data-hx-trigger="load"]'
 ).length;
 let loadedPartialsCount = 0;
 
 document.body.addEventListener("htmx:afterOnLoad", () => {
-    loadedPartialsCount++;
-    if (loadedPartialsCount === totalPartials) init();
+  loadedPartialsCount++;
+  if (loadedPartialsCount === totalPartials) init();
 });
