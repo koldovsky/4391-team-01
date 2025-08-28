e8b8f30caf1e1926d5b4d977fce84a6172e0b7cc
Renterl <krehovetskyy@gmail.com>
2025-08-28 15:19:16 +0300

    Fixed path to js file

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function init() {
     import("./global.header-burger.js");
     import("./features.js");
     import("./index.benefits-carousel.js");
-    import("./testimonials.index.js");
+    import("./index.testimonials.js");
     import("./index.footer-easter-egg.js");
     import("./index.footer-carousel.js");
     import("./index.footer-newsletter-form.js")
