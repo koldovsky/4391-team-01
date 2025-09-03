dc7bf47176e0f5322844c5810dafab15543bf66a
Renterl <krehovetskyy@gmail.com>
2025-09-03 22:30:32 +0300

    Added animation for opening and closing the modal window

--- js/index.promo-video.js
+++ js/index.promo-video.js
@@ -3,17 +3,16 @@ const closeBtn = document.querySelector(".promo-video__close-btn");
 const modal = document.querySelector(".promo-video__modal");
 const overlay = document.querySelector(".promo-video__overlay");
 
-playBtn.addEventListener("click", () => {
-  modal.style.display = "block";
-  overlay.style.display = "block";
-});
-closeBtn.addEventListener('click', () => {
-  modal.style.display = 'none';
-  overlay.style.display = 'none';
-});
+function modalWindowShow() {
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
 
-// клік по overlay теж закриває модалку
-overlay.addEventListener('click', () => {
-  modal.style.display = 'none';
-  overlay.style.display = 'none';
-});
+function modalWindowClose() {
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+playBtn.addEventListener("click", modalWindowShow);
+closeBtn.addEventListener("click", modalWindowClose);
+overlay.addEventListener("click", modalWindowClose);
