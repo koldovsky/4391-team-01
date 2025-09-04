69eda71c3d8998dec5d3eaf1271cd384caa86d0c
Bohdan Andriiovych <bodun1707@gmail.com>
2025-09-04 12:35:18 +0000

    ready version our-story

--- js/about-us.our-story.js
+++ js/about-us.our-story.js
@@ -1,18 +1,15 @@
-var acc = document.getElementsByClassName("clouds-types__button");
-var i;
+// Accordion for Clouds Types
+const acc = document.getElementsByClassName("clouds-types__button");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
+    const panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
       panel.style.display = "block";
     }
   });
-} 
+}
